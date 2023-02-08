79ec5a696c770fb6459b3aeb070ead31fb8a1fc2
AgeenkoKonstantin <kostik2000ag@gmail.com>
2023-02-08 17:20:16 +0300

    http server, test GET endpoint, starting cache realization, migrations:tables created

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"WB0/internal/apiserver"
+	"flag"
+	"github.com/BurntSushi/toml"
+	"log"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/server.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := apiserver.Start(config); err != nil {
+		log.Fatal(err)
+	}
+}
